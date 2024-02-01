480da91d54b377e152ca7380ad8a5733ebb9305b
newman0x <81893867+newman0x@users.noreply.github.com>
2024-01-31 20:49:20 -0800

    update api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,22 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+         <meta property="fc:frame" content="vNext" />
+         <meta property="fc:frame:image" content="https://i.imgur.com/iKbr2Nx.jpeg" />
+         <meta property="fc:frame:button:1" content="Green" />
+         <meta property="fc:frame:button:2" content="Purple" />
+         <meta property="fc:frame:button:3" content="Red" />
+         <meta property="fc:frame:button:4" content="Blue" />
+      </head>
+      <body></body>
+    </html>
+  `;
+
+  res.setHeader('Content-Type', 'text/html');
+  res.send(html);
 }
